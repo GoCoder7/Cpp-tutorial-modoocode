314e915ed13190af4274c98a21ebc1d66ff5a635
GoCoder <gocoder7@gmail.com>
2024-08-17 19:54:43 +0900

    8_lets_make_starcraft.cpp

---
+++ 8_lets_make_starcraft.cpp
@@ -0,0 +1,101 @@
+// https://modoocode.com/188
+#include <iostream>
+using std::string;
+
+class Marine {
+  int hp;                // 마린 체력
+  int coord_x, coord_y;  // 마린 위치
+  int damage;            // 공격력
+  bool is_dead;
+  string name;
+
+ public:
+  Marine();              // 기본 생성자
+  Marine(int x, int y);  // x, y 좌표에 마린 생성
+  Marine(int x, int y, string name); 
+  ~Marine() {
+    std::cout << name << " 의 소멸자 호출 !" << std::endl;
+  }
+
+  int attack();                       // 데미지를 리턴한다.
+  void be_attacked(int damage_earn);  // 입는 데미지
+  void move(int x, int y);            // 새로운 위치
+
+  void show_status();  // 상태를 보여준다.
+};
+Marine::Marine() {
+  hp = 50;
+  coord_x = coord_y = 0;
+  damage = 5;
+  is_dead = false;
+  name = "";
+}
+Marine::Marine(int x, int y) {
+  coord_x = x;
+  coord_y = y;
+  hp = 50;
+  damage = 5;
+  is_dead = false;
+  name = "";
+}
+Marine::Marine(int x, int y, string name) {
+  coord_x = x;
+  coord_y = y;
+  hp = 50;
+  damage = 5;
+  is_dead = false;
+  this->name = name;
+}
+void Marine::move(int x, int y) {
+  coord_x = x;
+  coord_y = y;
+}
+int Marine::attack() { return damage; }
+void Marine::be_attacked(int damage_earn) {
+  hp -= damage_earn;
+  if (hp <= 0) is_dead = true;
+}
+void Marine::show_status() {
+  std::cout << " *** Marine : " << name << " ***" << std::endl;
+  std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
+            << std::endl;
+  std::cout << " HP : " << hp << std::endl;
+}
+void each_marine(){
+  Marine marine0(2, 3);
+  Marine marine1(3, 5);
+
+  marine0.show_status();
+  marine1.show_status();
+
+  std::cout << std::endl << "마린 0 이 마린 1 를 공격! " << std::endl;
+  marine1.be_attacked(marine0.attack());
+
+  marine0.show_status();
+  marine1.show_status();
+}
+
+void marin_array() {
+  Marine* marines[100];
+
+  marines[0] = new Marine(2, 3, "Marine 0");
+  marines[1] = new Marine(3, 5, "Marine 1");
+
+  marines[0]->show_status();
+  marines[1]->show_status();
+
+  std::cout << std::endl << "마린 0 이 마린 1 를 공격! " << std::endl;
+  marines[1]->be_attacked(marines[0]->attack());
+
+  marines[0]->show_status();
+  marines[1]->show_status();
+
+  delete marines[0];
+  delete marines[1];
+}
+
+int main() {
+  each_marine();
+  marin_array();
+  return 0;
+}
