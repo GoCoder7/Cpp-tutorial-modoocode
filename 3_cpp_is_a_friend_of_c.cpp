c132513bdaec494f66cd707d6f0810570e5c5758
GoCoder <gocoder7@gmail.com>
2024-08-17 16:24:49 +0900

    3_cpp_is_a_friend_of_c

--- 3_cpp_is_a_friend_of_c.cpp
+++ 3_cpp_is_a_friend_of_c.cpp
@@ -9,20 +9,55 @@ void naming_convention() {
 
 void for_loop() {
   std::cout << std::endl;
-  std::array<int, 100> nums;
-  for (int i = 1; i<=100; i++) {
-    nums[i] = i;
-  };
   int sum = 0;
-  for (int i: nums) {
+  for (int i=1; i<=100; i++) {
     sum += i;
   };
   std::cout << "\nsum: " << sum << std::endl;
 }
 
+void while_loop() {
+  std::cout << std::endl;
+  int i = 0;
+  int sum = 0;
+  while (++i <= 100) {
+    sum += i;
+  };
+  std::cout << "\nsum: " << sum << std::endl;
+}
+
+void if_else() {
+  std::cout << std::endl;
+  int i = 57;
+  if (i < 50) {
+    std::cout << "i < 50" << std::endl;
+  } else if (i < 100) {
+    std::cout << "50 <= i < 100" << std::endl;
+  } else {
+    std::cout << "i >= 100" << std::endl;
+  }
+}
+
+void switch_case() {
+  std::cout << std::endl;
+  int i = 57;
+  switch (i) {
+    case 50:
+      std::cout << "i == 50" << std::endl;
+      break;
+    case 57:
+      std::cout << "i == 57" << std::endl;
+      break;
+    default:
+      std::cout << "i != 50 && i != 57" << std::endl;
+  }
+}
+
 int main() {
   naming_convention();
   for_loop();
+  while_loop();
+  if_else();
 
   return 0;
 }
